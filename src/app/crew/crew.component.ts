90f40ced188ae9cd1696233eb373390c2f99b522
Victoria <weddev2021vg@gmail.com>
2022-03-28 11:41:00 -0500

    css

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-crew',
   templateUrl: './crew.component.html',
-  styleUrls: ['./crew.component.css']
+  styleUrls: ['./crew.component.css'],
 })
 export class CrewComponent implements OnInit {
+  crew: string[] = ['Jessica Watkins', 'Raja Chari', 'Jasmin Moghbeli'];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
